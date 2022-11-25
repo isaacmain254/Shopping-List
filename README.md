ea48f6029131655d0f3ce21f8aae6258e55bb125
isaacmain254 <isaacmain72@gmail.com>
2022-11-25 18:10:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+To run the application
+First run the following command to install project packages
+
+npm install
+Then run the following to start the application:
+
+npm run
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { lazy, Suspense } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+import AppContext from "./context/AppContext";
+
+const Lists = lazy(() =>
+  import(/* webpackChunkName: "Lists"*/ "./pages/Lists")
+);
+
+const ListDetail = lazy(() =>
+  import(/* webpackChunkName: "ListDetail"*/ "./pages/ListDetail")
+);
+
+const ListForm = lazy(() =>
+  import(/* webpackChunkName: "ListForm"*/ "./pages/ListForm")
+);
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <BrowserRouter>
+          <Header />
+          <Suspense>
+            <AppContext fallback={<div>Loading ...</div>}>
+              <Routes>
+                <Route path="/" element={<Lists />} />
+                <Route path="/list/:listId/new" element={<ListForm />} />
+                <Route path="/list/:listId" element={<ListDetail />} />
+              </Routes>
+            </AppContext>
+          </Suspense>
+        </BrowserRouter>
+      </AppWrapper>
+    </>
+  );
+}
+export default App;
